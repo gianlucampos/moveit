daa0121393d75a564131637986becaa9404444a8
gianlucampos <gianlucampos97@gmail.com>
2021-03-28 18:10:33 -0300

    dia 3 nlw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
-import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -16,11 +17,12 @@ export default function Home() {
 
       <section>
         <div>
-          <Profile /> 
-          <CompletedChallenges/>
-          <Countdown/>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
+          <ChallengeBox />
         </div>
       </section>
     </div>
